5178ad3644c3aa50ef9ad7710e5b9a8b81075662
Tarun Kumar Singh <tarunsinghtech@gmail.com>
2025-06-27 17:46:19 +0530

    Refactor code structure for improved readability and maintainability

---
+++ Arrays/MaxMin.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int getMax(int num[], int n){
+    
+}
+
+int main()
+{
+    int size;
+    cout << "Enter the size of Array : ";
+    cin >> size;
+
+    int num[100];
+    cout << "Enter elements of array : " << endl;
+    for (int i = 0; i < size; i++)
+    {
+
+        cin >> num[i];
+    }
+
+    return 0;
+}
